cfc897963a423664cfe8c97e5d623a86572bcdb4
Rachel Chen <rachelchen0418@gmail.com>
2019-03-23 19:06:40 -0400

    embed typeform survey

--- src/components/survey.js
+++ src/components/survey.js
@@ -1,103 +1,10 @@
-/*import React, { Component } from 'react';
-import typeformEmbed from '@typeform/embed'
+import React from 'react';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
 
-class Survey extends Component {
-
-
-    componentDidMount(){
-
-        console.log(typeformEmbed);
-
-        const embedElement = document.querySelector('.js-typeform-embed')
-    
-        typeformEmbed.makeWidget(
-            embedElement,
-            'https://rachelchen2.typeform.com/to/P40bQl',
-            {
-              opacity: 55,
-              buttonText: "Answer this!",
-              hideScrollbars: true,
-              onSubmit: function () {
-                console.log('Typeform successfully submitted')
-              }
-            }
-          )
-
-        /*const popup1 = typeformEmbed.makePopup(
-        'https://rachelchen2.typeform.com/to/P40bQl',
-        {
-            mode: 'popup',
-            autoClose: 3000,
-            hideHeaders: true,
-            hideFooters: true,
-            onSubmit: function(){
-                console.log('Typeform successfully submitted')
-            }
-        }
-        )
-        document.getElementById('bt-popup').addEventListener('click', function(){
-            popup1.open();
-        });*/
-    //}
-
-    //render(){
-    //    return(
-    //        <div>
-    //            <button id='bt-popup' class='my-button'>popup</button>
-    //        </div>  
-    //    )
-    //}
-//}
-
-//export default Survey;
-
-import React, { Component } from 'react'
-//import typeformEmbed from '@typeform/embed'
-import * as typeformEmbed from '@typeform/embed'
-
-class Survey extends Component {
-    componentDidMount(){
-
-
-        const embedElement = document.querySelector('.js-typeform-embed')
-
-        typeformEmbed.makeWidget(
-            embedElement,
-            'https://rachelchen2.typeform.com/to/P40bQl',
-            {
-              opacity: 55,
-              buttonText: "Answer this!",
-              hideScrollbars: true,
-              onSubmit: function () {
-                console.log('Typeform successfully submitted')
-              }
-            }
-          )
-
-        // const popup1 = typeformEmbed.makePopup(
-        // 'https://rachelchen2.typeform.com/to/P40bQl',
-        // {
-        //     mode: 'popup',
-        //     autoClose: 3000,
-        //     hideHeaders: true,
-        //     hideFooters: true,
-        //     onSubmit: function(){
-        //         console.log('Typeform successfully submitted')
-        //     }
-        // }
-        // )
-        // document.getElementById('bt-popup').addEventListener('click', function(){
-        //     popup1.open();
-        // });
-    }
-
-    render(){
-        return(
-            <div>
-                <button id='bt-popup' class='my-button'>popup</button>
-            </div>  
-        )
-    }
+class Survey extends React.Component {
+  render() {
+    return <ReactTypeformEmbed url="https://rachelchen2.typeform.com/to/P40bQl" />;
+  }
 }
 
 export default Survey;
