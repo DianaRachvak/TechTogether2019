55a50380afa19271e41edb63af9dd50c45b67ba8
LENOVO-PC\diana_000 <mariaj4867@gmail.com>
2019-03-23 11:18:50 -0400

    login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: false
+    };
+  }
+
+  logMeIn = () => {
+    this.setState({isLoggedIn:true});
+    console.log(this.state.isLoggedIn);
+  }
+
   render() {
     return (
       <div className="App">
