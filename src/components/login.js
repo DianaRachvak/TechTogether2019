7b28a6515f09ac5f28a7dc10ae963d05b15437bf
LENOVO-PC\diana_000 <mariaj4867@gmail.com>
2019-03-24 03:02:12 -0400

    fixed menu and header

--- src/components/login.js
+++ src/components/login.js
@@ -77,59 +77,7 @@ class Login extends Component{
                       </MDBCard>
                   </MDBCol>
               </MDBRow>
-          </MDBContainer>
-        <MDBContainer className="d-flex justify-content-center">
-                <MDBRow>
-                    <MDBCol>
-                        <MDBCard>
-                            <MDBCardBody className="mx-4">
-                                <div className="text-center">
-                                    <h3 className="dark-grey-text mb-5">
-                                        <strong>Sign in</strong>
-                                    </h3>
-                                </div>
-                            <MDBInput 
-                            label="Your email"
-                            group
-                            type="email"
-                            validate
-                            error="wrong"
-                            success="right"
-                            //onChange={this.handleEmailChange} 
-                            />
-                            <MDBInput
-                            label="Your password"
-                            group
-                            type="password"
-                            validate
-                            containerClass="mb-0"
-                            //onChange={this.handlePasswordChange}
-                            />
-                            <div className="text-center mb-1">
-                            <MDBBtn
-                            type="button"
-                            gradient="blue"
-                            rounded
-                            className="btn-block z-depth-1a"
-                            //onClick={this.handleSubmit}
-                            >Sign in
-                            </MDBBtn>
-                            <Link to="/survey">
-                            <MDBBtn
-                            type="button"
-                            gradient="blue"
-                            rounded
-                            className="btn-block z-depth-1a"
-                            //onClick={this.handleSubmit} 
-                            >Sign Up
-                            </MDBBtn>
-                            </Link>
-                        </div>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-            </MDBRow>
-        </MDBContainer>
+          </MDBContainer>        
     </div>
     )
   }
