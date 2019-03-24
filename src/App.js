461c16cca6ea434861a8a6ded5b66fcb1a43761d
LENOVO-PC\diana_000 <mariaj4867@gmail.com>
2019-03-23 23:46:15 -0400

    fixed login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import LoginPage from './components/login'
+import LoginPage from './components/login';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import MenuOnLeft from './components/menuList';
+import HomePage from './components/homepage';
+import MatchingPage from './components/matchingpage';
 
 
 class App extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     isLoggedIn: false
-  //   };
-  // }
-
-  // logMeIn = () => {
-  //   this.setState({isLoggedIn:true});
-  //   console.log(this.state.isLoggedIn);
-  // }
-
   render() {
     return (
       <div className="App">
-         <LoginPage />
+         <Router>
+           <div>                         
+            <Route exact path='/' component={LoginPage}/>
+            <Route path='/menuList' component={MenuOnLeft}/>
+            <Route path='/homepage' component={HomePage}/>     
+            <Route path='/matchingpage' component={MatchingPage}/>
+           </div>
+         </Router>
       </div>
     );
   }
--- src/components/menuList.js
+++ src/components/menuList.js
@@ -1,11 +1,65 @@
-import React, { Component } from 'react'
-import { Grid, Menu, Segment } from 'semantic-ui-react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import MatchingPage from './matchingpage';
-import HomePage from '../components/homepage';
+// import React, { Component } from 'react'
+// import { Grid, Menu, Segment } from 'semantic-ui-react'
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+// import MatchingPage from './matchingpage';
+// import HomePage from '../components/homepage';
 
-export default class MenuOnLeft extends Component {
-  state = { activeItem: 'homepage' }
+// export default class MenuOnLeft extends Component {
+//   state = { activeItem: 'homepage' }
+
+//   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+//   render() {
+//     const { activeItem } = this.state
+
+//     return (
+//       <Router>
+//         <Grid>
+//           <Grid.Column width={4}>
+//             <Menu fluid vertical tabular>
+//               <Menu.Item
+//                 //as={Link} 
+//                 name='home'
+//                 //to='homepage'
+//                 active={activeItem === 'home'}
+//                 onClick={this.handleItemClick}
+//                 //<Link to={"/homepage"}>Home< /Link>
+//               />
+//               {/* <Menu.Item
+//                 name='profile'
+//                 active={activeItem === 'profile'}
+//                 onClick={this.handleItemClick}
+//               /> */}
+//               <Menu.Item
+//                 name='login'
+//                 active={activeItem === 'login'}
+//                 onClick={this.handleItemClick}
+//               />
+//               <Menu.Item
+//                 name='survey'
+//                 active={activeItem === 'survey'}
+//                 onClick={this.handleItemClick}
+//               />
+//             </Menu>
+//           </Grid.Column>
+
+//           <Grid.Column stretched width={12}>
+//             <Segment>
+//               This is an stretched grid column. 
+//               This segment will always match the tab height
+//             </Segment>
+//           </Grid.Column>
+//         </Grid>
+//       </Router>
+//     )
+//   }
+// }
+
+import React, { Component } from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
+
+export default class MenuExampleSecondaryPointing extends Component {
+  state = { activeItem: 'home' }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
@@ -13,48 +67,32 @@ export default class MenuOnLeft extends Component {
     const { activeItem } = this.state
 
     return (
-      <Router>
-        <Grid>
-          <Grid.Column width={4}>
-            <Menu fluid vertical tabular>
-              <Menu.Item
-                as={Link} 
-                name='Home'
-                to='homepage'
-                active={activeItem === 'Home'}
-                onClick={this.handleItemClick}
-                //<Link to={"/homepage"}>Home< /Link>
-                //<Link to="/react">React</Link>
-              />
-              <Menu.Item
-                as={Link} to='/profile'
-                name='Profile'
-                active={activeItem === 'profile'}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                as={Link} to='/tips'
-                name='Tips'
-                active={activeItem === 'tips'}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                as={Link} to='/matchingpage'
-                name='Match'
-                active={activeItem === 'matchingpage'}
-                onClick={this.handleItemClick}
-              />
-            </Menu>
-          </Grid.Column>
+      <div>
+        <Menu pointing secondary>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item
+            name='messages'
+            active={activeItem === 'messages'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='friends'
+            active={activeItem === 'friends'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='logout'
+              active={activeItem === 'logout'}
+              onClick={this.handleItemClick}
+            />
+          </Menu.Menu>
+        </Menu>
 
-          <Grid.Column stretched width={12}>
-            <Segment>
-              This is an stretched grid column. 
-              This segment will always match the tab height
-            </Segment>
-          </Grid.Column>
-        </Grid>
-      </Router>
+        <Segment>
+          <img src='/images/wireframe/media-paragraph.png' />
+        </Segment>
+      </div>
     )
   }
 }
